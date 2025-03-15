7e5a83b0f79dbfc534589041cc4923500229bf22
рума <elchaninapolina@gmail.com>
2025-03-15 16:21:54 +0700

    Rendering lists

--- vue/js/main.js
+++ vue/js/main.js
@@ -6,6 +6,19 @@ let app = new Vue({
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         onSale: false,
-        inventory: 100
+        inventory: 100,
+        details: ['80% cotton', '20% polyesyer', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+
     }
 })
