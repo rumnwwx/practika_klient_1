dd19eb9e16c87a7d547bf77761b2830f669ecc58
рума <elchaninapolina@gmail.com>
2025-03-17 18:44:32 +0700

    Event Handling

--- vue/js/main.js
+++ vue/js/main.js
@@ -4,21 +4,32 @@ let app = new Vue({
         product: "Socks",
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         onSale: false,
         inventory: 100,
         details: ['80% cotton', '20% polyesyer', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-
+        cart: 0
+    },
+    methods: {
+        addToCard(){
+            this.cart += 1
+        },
+        removeFromCard() {
+            this.cart -= 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
     }
 })
